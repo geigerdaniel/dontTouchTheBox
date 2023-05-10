ff614061908a2091a5c79906af78201b073cdc20
USER <a@a.a>
2023-05-10 19:21:17 +0300

    added the png

--- don't touch the box.py
+++ don't touch the box.py
@@ -13,14 +13,17 @@ yellowBoxXPosition = 300
 yellowBoxYPosition = 300
 yellowBoxXLength = 400
 yellowBoxYLength = 400
+yellowBox = Rect(yellowBoxXPosition, yellowBoxYPosition, yellowBoxXLength, yellowBoxYLength)
+
+redBox = Rect(400, 400, 200, 200)
 
 dotXLength = 20
 dotYLength = 20
 whiteDot = pygame.transform.scale(originalWhiteDot, (dotXLength, dotYLength))
 dotXPosition = 0
 dotYPosition = 0
-initialXPosition = [0]
-initialYPosition = [0]
+initialXPosition = 0
+initialYPosition = 0
 
 clock = pygame.time.Clock()
 
@@ -42,30 +45,40 @@ while True:
                 timerRunning = False  # stop the timer
                 canCheckForDirection = True  # allow direction checking immediately
                 targetTime = None  # reset the target time to None
-                print("test")
+                print("test2")
 
             else:
                 timerRunning = True
-                targetTime = time.time() + 3  # set the target time for the timer
+                targetTime = time.time() + 0.1  # set the target time for the timer
                 canCheckForDirection = False
                 initialXPosition = dotXPosition
-                initialYPosition = frozenset([dotYPosition])
-
+                initialYPosition = dotYPosition
+                print("test3")
 
     # Check if the target time has passed and print "test" if it has
     if targetTime is not None and time.time() >= targetTime:
-        print("test")
+        print("test initial X = " + str(initialXPosition) + ", Y = " + str(initialYPosition) + " dot X = " + str(
+            dotXPosition) + ", Y = " + str(dotYPosition))
+        m = (dotYPosition - initialYPosition) / (dotXPosition - initialXPosition)
+        c = dotYPosition - (m * dotXPosition)
+
+        leftCollisionY = m*redBox.left + c
+        rightCollisionY = m*redBox.right + c
+
+        if redBox.top <= leftCollisionY <= redBox.bottom or redBox.topright <= rightCollisionY <= redBox.bottomright:
+            print("oh no!!!")
+        else:
+            print("nice")
+
         timerRunning = False
         canCheckForDirection = False
         targetTime = None
 
     pygame.draw.rect(canvas, (10, 10, 40), [0, 0, 1000, 1000])
-    pygame.draw.rect(canvas, (200, 200, 0),
-                     [yellowBoxXPosition, yellowBoxYPosition, yellowBoxXLength, yellowBoxYLength])
-    pygame.draw.rect(canvas, (255, 0, 0), [400, 400, 200, 200])
+    pygame.draw.rect(canvas, (200, 200, 0), yellowBox)
+    pygame.draw.rect(canvas, (255, 0, 0), redBox)
     canvas.blit(whiteDot, (dotXPosition, dotYPosition))
 
-    yellowBox = Rect(yellowBoxXPosition, yellowBoxYPosition, yellowBoxXLength, yellowBoxYLength)
     whiteDotRect = Rect(dotXPosition, dotYPosition, dotXLength, dotYLength)
     dotToYellowCollide = pygame.Rect.colliderect(whiteDotRect, yellowBox)
     if dotToYellowCollide:
@@ -84,8 +97,8 @@ while True:
         dotXPosition -= 1
     if keyPressed[pygame.K_d] and dotXPosition < 1000 - dotXLength - 1:
         dotXPosition += 1
-    print(initialXPosition[0])
-    print(initialYPosition[0])
+    print(initialXPosition)
+    print(initialYPosition)
 
     pygame.display.update()
     pygame.display.flip()
